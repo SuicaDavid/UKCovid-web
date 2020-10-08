e4b7292de993019258da3da8d2168b72c41c699e
David <genglanghuang@gmail.com>
2020-10-08 02:10:14 +0100

    complete the animation of searching bar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -9,34 +9,46 @@ export default class Search extends Component {
     state = {
         isSearching: false
     }
+
     constructor() {
-        super();
+        super()
         this.onClickSearchInput = this.onClickSearchInput.bind(this)
     }
 
     onClickSearchInput(e) {
-        console.log(e.target.className)
+        e.stopPropagation()
+        if (this.state.isSearching && e.target.className === 'search-input-text') {
+            return
+        }
         this.setState({isSearching: !this.state.isSearching})
     }
+
     render() {
         return (
             <div className="search-view">
-                <div className={`${this.state.isSearching ? 'search-input-view-selected' : 'search-input-view'}`} onClick={this.onClickSearchInput}>
-                    <div className={`search-input-container`}>
-                        <input type="text" placeholder="Input your city" className="search-input-text" disabled={!this.state.isSearching}/>
-                        <img src={searchIcon} alt="search icon" className="search-icon"/>
+                <div className="search-view-row">
+                    <div className={`${this.state.isSearching ? 'search-input-view-selected' : 'search-input-view'}`}
+                         onClick={this.onClickSearchInput}>
+                        <div
+                            className={this.state.isSearching ? 'search-input-container-selected' : 'search-input-container'}>
+                            <input type="text" placeholder="Input your city" className="search-input-text"
+                                   disabled={!this.state.isSearching}/>
+                            <img src={searchIcon} alt="search icon" className="search-icon"/>
+                        </div>
                     </div>
                 </div>
-                <div className="search-current-city-view">
-                    <h2 className="search-current-city-name">London</h2>
-                    <div className="search-current-city-cases">
-                        <div className="search-current-city-today-cases">
-                            <h4>cases today</h4>
-                            <h3>1000</h3>
-                        </div>
-                        <div className="search-current-city-total-cases">
-                            <h4>cases total</h4>
-                            <h3>10000</h3>
+                <div className="search-view-row">
+                    <div className="search-current-city-view">
+                        <h2 className="search-current-city-name">London</h2>
+                        <div className="search-current-city-cases">
+                            <div className="search-current-city-today-cases">
+                                <h4>cases today</h4>
+                                <h3>1000</h3>
+                            </div>
+                            <div className="search-current-city-total-cases">
+                                <h4>cases total</h4>
+                                <h3>10000</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
